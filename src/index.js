4093bbf8cee18f55276db1f63a5d7e4058c22517
OliviavP <livalossy@gmail.com>
2022-06-06 15:05:53 -0600

    Finished Projects

--- src/index.js
+++ src/index.js
@@ -1,42 +1,114 @@
-const siteContent = { // DO NOT CHANGE THIS OBJECT
-  "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
+const siteContent = {
+  // DO NOT CHANGE THIS OBJECT
+  nav: {
+    'nav-item-1': 'Services',
+    'nav-item-2': 'Product',
+    'nav-item-3': 'Vision',
+    'nav-item-4': 'Features',
+    'nav-item-5': 'About',
+    'nav-item-6': 'Contact',
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
+  cta: {
+    h1: 'DOM Is Awesome',
+    button: 'Get Started',
   },
-  "main-content": {
-    "features-h4": "Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4": "About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "services-h4": "Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4": "Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4": "Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  'main-content': {
+    'features-h4': 'Features',
+    'features-content':
+      'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'about-h4': 'About',
+    'about-content':
+      'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'services-h4': 'Services',
+    'services-content':
+      'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'product-h4': 'Product',
+    'product-content':
+      'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+    'vision-h4': 'Vision',
+    'vision-content':
+      'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
   },
-  "contact": {
-    "contact-h4": "Contact",
-    "address": "123 Way 456 Street Somewhere, USA",
-    "phone": "1 (888) 888-8888",
-    "email": "sales@greatidea.io",
+  contact: {
+    'contact-h4': 'Contact',
+    address: '123 Way 456 Street Somewhere, USA',
+    phone: '1 (888) 888-8888',
+    email: 'sales@greatidea.io',
   },
-  "footer": {
-    "copyright": "Copyright Great Idea! 2021",
+  footer: {
+    copyright: 'Copyright Great Idea! 2021',
   },
-  "images": {
-    "logo-img": "http://localhost:9000/img/logo.png",
-    "cta-img": "http://localhost:9000/img/cta.png",
-    "accent-img": "http://localhost:9000/img/accent.png",
+  images: {
+    'logo-img': 'http://localhost:9000/img/logo.png',
+    'cta-img': 'http://localhost:9000/img/cta.png',
+    'accent-img': 'http://localhost:9000/img/accent.png',
   },
-};
+}
 
 console.log('project wired!')
+
+//Nav
+
+const links = document.querySelectorAll('header nav a')
+const data = Object.values(siteContent.nav)
+links.forEach((link, idx) => {
+  link.textContent = data[idx]
+  link.classList.add('italic')
+})
+
+//Body
+
+const topContent = document.querySelector('.top-content')
+topContent.children[0].children[0].textContent =
+  siteContent['main-content']['features-h4']
+topContent.children[0].children[1].textContent =
+  siteContent['main-content']['features-content']
+topContent.children[1].children[0].textContent =
+  siteContent['main-content']['about-h4']
+topContent.children[1].children[1].textContent =
+  siteContent['main-content']['about-content']
+
+const bottomCon = document.querySelector('.bottom-content')
+bottomCon.children[0].children[0].textContent =
+  siteContent['main-content']['services-h4']
+bottomCon.children[0].children[1].textContent =
+  siteContent['main-content']['services-content']
+bottomCon.children[1].children[0].textContent =
+  siteContent['main-content']['product-h4']
+bottomCon.children[1].children[1].textContent =
+  siteContent['main-content']['product-content']
+bottomCon.children[2].children[0].textContent =
+  siteContent['main-content']['vision-h4']
+bottomCon.children[2].children[1].textContent =
+  siteContent['main-content']['vision-content']
+
+//Images
+
+const logo = document.querySelector('#logo-img')
+logo.src = siteContent.images['logo-img']
+
+const cta = document.querySelector('#cta-img')
+cta.src = siteContent.images['cta-img']
+
+const middleImg = document.querySelector('#middle-img')
+middleImg.setAttribute('src', siteContent.images['accent-img'])
+
+//CTA
+
+document.querySelector(' .cta .cta-text h1').textContent = siteContent.cta.h1
+document.querySelector(' .cta .cta-text button').textContent =
+  siteContent.cta.button
+
+//Contact
+
+const contact = document.querySelector('section.contact')
+contact.children[0].textContent = siteContent.contact['contact-h4']
+contact.children[1].textContent = siteContent.contact['address']
+contact.children[2].textContent = siteContent.contact['phone']
+contact.children[3].textContent = siteContent.contact['email']
+
+//Footer
+
+const footer = document.querySelector('footer a')
+footer.textContent = siteContent.footer.copyright
+footer.classList.add('bold')
